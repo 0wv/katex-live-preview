d71365fe707f9e25e7021de51bfe4c2bbb5d981f
0wv <w32w64@gmail.com>
2021-05-24 08:34:07 +0000

    :construction: implement logic

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,25 @@
 <script>
-	export let name;
+	import { beforeUpdate } from 'svelte'
+	import katex from 'katex'
+	import '../node_modules/katex/dist/katex.css'
+
+	let katexRaw = ''
+	let renderedKatex
+
+	beforeUpdate(() => {
+		let katexString = katex.renderToString(katexRaw, {
+			output: 'html',
+			throwOnError: false,
+		})
+		renderedKatex = katexString
+	})
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<textarea bind:value={katexRaw}></textarea>
+	<div bind:innerHTML={renderedKatex} contenteditable></div>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+	/* pass */
 </style>
