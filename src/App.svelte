5fe1151f4f2990c6dea78020c36af00971cabc05
0wv <w32w64@gmail.com>
2021-05-26 13:00:33 +0000

    :lipstick: remove unnecessary padding

--- src/App.svelte
+++ src/App.svelte
@@ -21,11 +21,11 @@
 
 <main>
 	<div class="bg-blue-100 m-4 p-4 rounded-2xl">
-		<label class="border-b-4 border-blue-400 font-bold mb-2 ml-4 mt-4 text-lg w-max" for="katexRawElem">数式</label>
+		<label class="border-b-4 border-blue-400 font-bold mb-2 ml-4 text-lg w-max" for="katexRawElem">数式</label>
 		<div class="mb-4 mt-4 pl-4 pr-4">
 			<textarea bind:value={katexRaw} class="border-0 font-mono p-4 rounded-2xl w-full" placeholder="ここに数式を入力してみよう！ (例: \frac{'{'}1{'}'}{'{'}2{'}'})" id="katexRawElem" rows="5"></textarea>
 		</div>
-		<span class="border-b-4 border-green-400 font-bold mb-2 ml-4 mt-4 text-lg w-max">出力</span>
+		<span class="border-b-4 border-green-400 font-bold mb-2 ml-4 text-lg w-max">出力</span>
 		{#if katexRaw !== ''}
 			<div bind:innerHTML={renderedKatex} class="bg-white m-4 p-4 rounded-2xl" contenteditable></div>
 		{:else}
